80577964aef3287e8be7c670507fb8870aa11f01
yesharry <reerohh@gmail.com>
2022-11-17 17:10:04 +0900

    rechart piechart 라이브러리 test 적용

--- src/pages/Vic.tsx
+++ src/pages/Vic.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import CollapsibleTable from './components/CollapsibleTable';
+import PieChartTest from './components/PieChartTest';
 
 const Vic = () => {
   return (
     <Div>
       <CollapsibleTable />
+      <PieChartTest />
     </Div>
   );
 };
