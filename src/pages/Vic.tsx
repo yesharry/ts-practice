78cf0e4406abc1b46313d04217aba553ffa183c0
yesharry <reerohh@gmail.com>
2022-11-17 16:49:48 +0900

    mui table 라이브러리 test 적용

--- src/pages/Vic.tsx
+++ src/pages/Vic.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import CollapsibleTable from './components/CollapsibleTable';
 
 const Vic = () => {
-  return <div>Vic</div>;
+  return (
+    <Div>
+      <CollapsibleTable />
+    </Div>
+  );
 };
+
+const Div = styled.div``;
 
 export default Vic;
